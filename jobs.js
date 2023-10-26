4868d7db5d223a056cfc13e77ef32464e7e15272
wei-harness <75070418+wei-harness@users.noreply.github.com>
2023-10-26 11:52:55 +0100

    Update jobs.js

--- jobs.js
+++ jobs.js
@@ -1,9 +1,10 @@
+console.log('Loading jobs...')
 let jobs = []
 async function test() {
-const response = await fetch('https://boards-api.greenhouse.io/v1/boards/harnessinc/jobs')
-const data = await response.json()
-console.log(data)
-jobs = data.jobs
+  const response = await fetch('https://boards-api.greenhouse.io/v1/boards/harnessinc/jobs')
+  const data = await response.json()
+  console.log(data)
+  jobs = data.jobs
 }
 test()
-const sampleText = 'hello world!'
+const sampleText = 'sample job!'
