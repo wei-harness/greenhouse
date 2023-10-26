2053cbcdd7a46ec8b35d5b5e13ceea702bdcd01b
wei-harness <75070418+wei-harness@users.noreply.github.com>
2023-10-26 13:42:18 +0100

    Update jobs.js

--- jobs.js
+++ jobs.js
@@ -1,29 +1,34 @@
 console.log('Initiating jobs page...');
-// let jobs = [];
 async function getJobs() {
   console.log('Fetching jobs...');
+  const epJobs = "https://boards-api.greenhouse.io/v1/boards/harnessinc/jobs?content=true";
+  // const epDepartments = "https://boards-api.greenhouse.io/v1/boards/harnessinc/departments?render_as=tree"; // department data is hierarchical
   const response = await fetch('https://boards-api.greenhouse.io/v1/boards/harnessinc/jobs');
   const data = await response.json();
-  console.log(data);
   const jobs = data.jobs;
+  // const departments = data.departments;
 
+  /* get list/item templates */
+  // const jobItem = document.getElementById('gh_department_item');
+  // const jobList = document.getElementById('gh_department_list');
   const jobItem = document.getElementById('gh_job_item');
   const jobList = document.getElementById('gh_job_list');
+  
   jobs.forEach(job => {
     const newJobItem = jobItem.cloneNode(true);
     const jobLink = newJobItem.getElementsByTagName('a')[0];
-    //title
+    /* title */
     jobLink.innerText = job.title ? `${job.title} (${job.location.name})` : 'Untitled';
-    //absolute_url
+    /* absolute_url */
     jobLink.setAttribute('href', job.absolute_url || '#')
-    //location.name
+    /* location.name */
     jobList.appendChild(newJobItem);
   });
-  // remove job item template
+  /* remove job item template */
   jobList.removeChild(jobItem);
 }
 
-// better to invoke it after document.ready
+/* better to invoke it after document.ready */
 getJobs();
 
 /* jquery not available here
