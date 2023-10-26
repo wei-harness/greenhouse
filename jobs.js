1e6ad66cd21e67706fb080ebbd976eced7356a9b
wei-harness <75070418+wei-harness@users.noreply.github.com>
2023-10-26 12:47:15 +0100

    Update jobs.js

--- jobs.js
+++ jobs.js
@@ -1,18 +1,31 @@
 console.log('Initiating jobs...');
 let jobs = [];
-async function test() {
+async function getJobs() {
   console.log('Loading jobs...');
   const response = await fetch('https://boards-api.greenhouse.io/v1/boards/harnessinc/jobs');
   const data = await response.json();
   console.log(data);
   jobs = data.jobs;
+
+  const jobItem = document.getElementById('gh_job_item');
+  const jobList = document.getElementById('gh_job_list');
+  jobs.forEach(job => {
+    const newJobItem = jobItem.cloneNode(true);
+    const jobLink = newJobItem.getElementsByTagName('a')[0];
+    //title
+    jobLink.innerText = job.title ? `${job.title} (${location.name})` : 'Untitled';
+    //absolute_url
+    jobLink.setAttribute('href', job.absolute_url || '#')
+    //location.name
+    jobList.appendChild(newJobItem);
+  })
 }
 
-test();
+getJobs();
 
 const sampleText = 'sample job!';
 
 $( document ).ready(() => {
   console.log('Jobs doc ready...');
-  test();
+  getJobs();
 });
